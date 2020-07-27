7b0e81752a78b2c5650e13113003aa305b11860f
Kitcham <kitcham@outlook.com>
2020-07-27 23:27:13 +0800

    refactor(all) : remove old files
    Remove all files.

--- dbconfig.php
+++
@@ -1,7 +0,0 @@
-<?php
-define(HOST,"localhost");	  //host
-define(DBNAME,"blogtest");     //dbname
-define(ROOT,"root");          //user
-define(PASSWORD,"rootroot");  //password
-define(DSN,"mysql:host=".HOST.";dbname=".DBNAME);
-?>
--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-header("content-type:text/html;charset=utf-8");
-session_start();
-$_SESSION=array();
-setcookie(PHPSESSID,'',time()-1,'/');
-session_destroy();
-echo "<script>location='index.php'</script>";
-?>

44667e5ad175a3ac38e947a614c8ba3688852361
Kitcham <kitcham@outlook.com>
2020-07-27 21:17:20 +0800

    refactor(all) : remove project uploaded by mistake
    Remove all files for mistaken upload.

--- dbconfig.php
+++
@@ -1,7 +0,0 @@
-<?php
-define(HOST,"localhost");	  //host
-define(DBNAME,"healthcodetest1");     //dbname
-define(ROOT,"root");          //user
-define(PASSWORD,"rootroot");  //password
-define(DSN,"mysql:host=".HOST.";dbname=".DBNAME);
-?>
